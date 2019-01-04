f576fef6ada34214d4c09bd632c923dc3f7a4ed0
Hankyu Henry Lee <aicies119@gmail.com>
2019-01-04 23:39:48 +0900

    User list 액티비티에 SwipeRefreshLayout 적용 완료.

--- app/src/main/java/pe/hankyu/svmgithubbrowser/MainActivity.kt
+++ app/src/main/java/pe/hankyu/svmgithubbrowser/MainActivity.kt
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
@@ -17,6 +20,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        compositeDisposable = CompositeDisposable()
+
+        compositeDisposable.add(GithubApi.getUserList("0")
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.newThread())
+            .subscribe ({ response: List<GithubUserModel> ->
+                val userAdapter = UserAdapter(response)
+                user_recyclerview.adapter = userAdapter
+            }, { error: Throwable ->
+                Log.d("MainActivity", error.localizedMessage)
+                Toast.makeText(this, "네트워크 오류입니다.", Toast.LENGTH_SHORT).show();
+            }))
+
+        user_swipelayout.setOnRefreshListener(this::onRefresh)
     }
 
     override fun onResume() {
@@ -25,16 +43,25 @@ class MainActivity : AppCompatActivity() {
         val mLayoutManager = LinearLayoutManager(this)
         user_recyclerview.layoutManager = mLayoutManager
 
+
+    }
+
+    private fun onRefresh() {
         compositeDisposable = CompositeDisposable()
 
-        compositeDisposable.add(GithubApi.getUserList("0")
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.newThread())
-                .subscribe ({ response: List<GithubUserModel> ->
-                    val userAdapter = UserAdapter(response)
-                    user_recyclerview.adapter = userAdapter
-                }, { error: Throwable ->
-                    Log.d("MainActivity", error.localizedMessage)
-                }))
+        compositeDisposable.add(GithubApi.getUserList("46")
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.newThread())
+            .subscribe ({ response: List<GithubUserModel> ->
+                val userAdapter = UserAdapter(response)
+                user_recyclerview.adapter = userAdapter
+
+                user_swipelayout.isRefreshing = false
+            }, { error: Throwable ->
+                Log.d("MainActivity", error.localizedMessage)
+                Toast.makeText(this, "네트워크 오류입니다.", Toast.LENGTH_SHORT).show();
+
+                user_swipelayout.isRefreshing = false
+            }))
     }
 }
